c7d5aa5406d75efcef74522c8b7ac976270e350b
saurabhoatstech <saurabhnegi0007@gmail.com>
2023-11-04 23:54:47 +0530

    restaurant name, cuisine extracted

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,6 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Restaurant_Menu_API } from "../utils/constants";
+import Shimmer from "./Shimmer";
 const RestaurantMenu = () => {
+  const [resInfo, setResInfo] = useState(null);
   useEffect(() => {
     fetchData();
   }, []);
@@ -9,13 +11,23 @@ const RestaurantMenu = () => {
     const data = await fetch(Restaurant_Menu_API);
     const json = await data.json();
 
+    setResInfo(json.data);
     console.log(json.data);
   };
 
+  if (resInfo === null) {
+    return <Shimmer />;
+  }
+
+  const { cuisines, name } = resInfo?.cards[0]?.card?.card?.info;
+
+  // const {itemCards} = resInfo?.cards[3].groupedCard
+
   return (
     <div>
-      RestaurantMenu
-      <p>Menu</p>
+      <h1>{name}</h1>
+      <p>{cuisines && cuisines.join(", ")}</p>
+      <h2>Menu</h2>
     </div>
   );
 };
